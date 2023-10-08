8ab8ff9bbac74047a9cdf039883749630269ce4c
Arsen <chabolaxavi@gmail.com>
2023-10-08 14:43:54 +0200

    Added calc method (calculate temperatures)

--- src/Temperature.java
+++ src/Temperature.java
@@ -23,7 +23,31 @@ public class Temperature {
         }
     }
 
-    public void printTemps(){
+    private void calc() {
+        int avgTemp;
+        for (int row = width-1; row >= 0; row--) {
+            System.out.println("hola");
+            for (int col = 0; col < height; col++) {
+                if (row != 0) {
+                    if (col == 0) {
+                        avgTemp = (temps[row][col] + temps[row][col+1] + temps[row-1][col]) / 3;
+                    }
+                    else if ((col == height-1)) {
+                        avgTemp = (temps[row][col] + temps[row][col-1] + temps[row-1][col]) / 3;
+                    }
+                    else {
+                        avgTemp = (temps[row][col-1] + temps[row][col] + temps[row][col+1] + temps[row-1][col]) / 4;
+                    }
+
+                    System.out.println("CurrentPos: " + temps[row][col]);
+                    System.out.println("AvgTemp: " + avgTemp);
+                    temps[row-1][col] = avgTemp;
+                }
+            }
+        }
+    }
+
+    private void printTemps(){
         for (int i = 0; i < height; i++) {
             for (int j = 0; j < width; j++) {
                 if (temps[i][j] != 0) {
@@ -37,9 +61,10 @@ public class Temperature {
     }
 
     public static void main(String[] args) {
-        Temperature tempsMap = new Temperature(10,10,0.3, 0.9);
+        Temperature tempsMap = new Temperature(10,10,0.3, 0.3);
 
         tempsMap.sparks();
+        tempsMap.calc();
         tempsMap.printTemps();
     }
 }
